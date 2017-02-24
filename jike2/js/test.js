b104bc37b1c3588f0deda23289c7aacca1abcff1
xueweicong <595215306@qq.com>
2017-02-24 13:05:05 +0800

    Signed-off-by: xueweicong <595215306@qq.com>

---
+++ jike2/js/test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by xwc on 2017/2/15.
+ */
+var x = 10;
+var y = 20;
+function fn () {
+    console.log(x);
+    console.log(y);
+    var x = 5;
+}
+
+
+var s1 = "some text";
+var s2 = s1.substring(2);
+console.log(s1,s2);
